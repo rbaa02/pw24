809dd7b6ee874bbd5d13ef55131d1fff3a665361
Raphael Brandão <113994733+rbaa02@users.noreply.github.com>
2024-09-26 12:31:27 -0300

    Add files via upload

---
+++ php/atv1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>bah</title>
+</head>
+<body>
+	<?php
+		echo "<img src='https://picsum.photos/500/600' alt=''>", "<h1>Imagem Aleatória<h1>";
+	?>
+</body>
+</html>
